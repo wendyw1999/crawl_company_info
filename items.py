f98fec7667119b83b9146e8b97a3e55de63afa51
Caiwei Wang <43483829+wendyw1999@users.noreply.github.com>
2020-09-07 10:26:50 +0800

    information needed to crawl

---
+++ items.py
@@ -0,0 +1,36 @@
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://docs.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class CaseItem(scrapy.Item):
+    AGE = scrapy.Field()
+    AREA_ID= scrapy.Field()
+    AREA_NAME= scrapy.Field()
+    BUESINESSENTITY= scrapy.Field()
+    CARDNUM= scrapy.Field()
+    CARDNUMBER= scrapy.Field()
+    CARDTYPE= scrapy.Field()
+    CASE_CODE= scrapy.Field()
+    COURT_NAME= scrapy.Field()
+    DISREPUT_TYPE_NAME= scrapy.Field()
+    DUTY= scrapy.Field()
+    GIST_CID= scrapy.Field()
+    GIST_UNIT= scrapy.Field()
+    ID= scrapy.Field()
+    INAME= scrapy.Field()
+    PARTY_TYPE_NAME= scrapy.Field()
+    PERFORMANCE= scrapy.Field()
+    PERFORMED_PART= scrapy.Field()
+    PUBLISH_DATE= scrapy.Field()
+    REG_DATE= scrapy.Field()
+    SEX_NAME= scrapy.Field()
+    TRANS_DM_TONG_INSERTDATE= scrapy.Field()
+    UNPERFORM_PART= scrapy.Field()
+
+    
+    
+    
